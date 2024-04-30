03eaeff0ee79dabfa8cc67b7b3e481160486e807
sachinhere33 <162969069+sachinhere33@users.noreply.github.com>
2024-04-30 07:26:12 +0530

    added active users functionality

--- client/components/ChatHeader.jsx
+++ client/components/ChatHeader.jsx
@@ -1,15 +1,25 @@
-import React from "react"
-const ChatHeader = () => {
+import React,{useEffect, useState} from "react"
+const ChatHeader = ({socket}) => {
+    const [users,setUsers] = useState([])
+useEffect(()=>{
+    socket.on("newUserResponse",(data)=>setUsers(data))
+    
+},[socket,users])
+
+console.log(users)
     return (
         <div className="chat__sidebar">
             <h2>open chat</h2>
             <div>
-                <h4 className="chat__header">online users</h4>
+                <h4 className="chat__header">acitve users</h4>
                 <div className="chat__users">
-                    <p>user 1</p>
-                    <p>user 2</p>
-                    <p>user 3</p>
-                    <p>user 4</p>
+                   
+{users.map((user)=>(<p key={user.socketID}>{user.userName}</p>))}
+
+
+
+
+
                 </div>
             </div>
         </div>
